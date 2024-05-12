c3ac5509dc3b0c5db1b515b03d82d9703a740e7d
nastorvlad <llkdjjs@gmail.com>
2024-05-12 19:50:17 +0300

    Почистили старые комментарии 123123

--- myscript.js
+++ myscript.js
@@ -1,392 +1,3 @@
-// alert("Вася АААААААА")
-
-// prompt("Как дела?")
-// confirm("Ходил сегодня в магаз?")
-let name = 'Тут был Вася'
-let old = 16
-
-// alert("Тут был " + name + " и ему " + old + ' лет')
-// alert(`Тут был ${name} и ему ${old} лет`)
-// Тут был djdjd и ему 16 лет    
-// + ' лет'
-
-// Тут небольшой рабочий кусок программы
-// let old = prompt("Сколько тебе лет?")
-// let name = prompt("Как тебя зовут?")
-
-// console.log(`Тут был ${name} и ему ${old} лет`)
-
-
-
-// let number1 = 12
-// let number2 = 7
-// let result = number1 + number2
-
-// alert(number1 + number2)
-// alert(result)
-
-// let num = 245
-
-// alert(num * num)
-// let b = 40
-// let name = "Вася"
-// let y = 250/(2 - 40 + 15)
-// alert(y)
-
-
-// let number1 = prompt("Ввидите первое число")
-// console.log("первое число", number1)
-
-// let number2 = prompt("Ввидите второе число")
-// console.log("второе число", number2)
-
-// number1 = Number(number1)
-// number2 = Number(number2)
-
-// console.log("первое число", number1)
-// console.log("второе число", number2)
-
-// alert("В сумме выходит " + (number1 + number2))
-
-
-// let names = ["Вася", "Петя", "Коля"]
-// console.log(names);
-
-// let games = ["Rust", "Warzone", "Letal Company"]
-// console.log(games)
-
-// let numbers = [342, 73.5, 214.374719, 854, 122]
-// console.log(numbers)
-
-// В сумме выхоит ?????
-
-
-
-// let num = prompt("Ввидите любое число")
-// alert(num * num * num)
-
-// let name = "Vasya"
-// let num2 = 50
-// let num3 = "50"
-
-
-//let num = prompt("Ввидите любое число") // 260
-// num = Number("260") + Number("260")
-// alert(num)
-
-
-// let num = prompt("Ввидите любое число") // 260
-// num = Number("50") + Number("260")
-// alert(num)
-
-// Number("50") --> 50
-// Number("260") --> 260
-
-// num = "50" + "260"  "26050"
-//num = Number("50") + Number("260") = 50 + 260 = 310
-
-// let num = prompt("Ввидите любое число") // 260
-// num = "260"
-//"260" ---> 260
-//num = Number("260")
-// num = Number(num)
-//  alert(Number(num) + Number(num))
-//  alert(Number(num + num))
-// alert(num + num)
-
-
-//введи 2 числа, сложи их , умнож полученный результат на 2, покажи его на экране
-
-// let num1 = prompt("Введите первое число") // 400
-// let num2 = prompt("Введите второе число") // 700
-// let num3 = ( Number(num1) + Number (num2) ) * 2          // 1100
-// //let num4 = num3 * 2 // 2200
-// alert(num3)
-
-
-//let num1 = Number( prompt("Введите первое число") ) // 400
-//let num2 = Number( prompt("Введите второе число") ) // 700
-//let num3 = ( num1 + num2 ) * 2          // 1100
-//let num4 = num3 * 2 // 2200
-//alert(num3)
-
-//дано три числа. Сложить их между собой, увеличить в десять раз, отнять 5. показать резульат
-
-// let num1 = Number(prompt("Введите первое число"))
-// let num2 = Number(prompt("Введите второе число"))
-// let num3 = Number(prompt("Введите третье число"))
-// let num4 = ( num1 + num2 + num3 ) * 10 - 5
-// alert(num4)
-
-
-// let userName = "dima"
-
-// if(userName == "vovan223")
-//     alert("Привет вован")
-// else
-//     alert("Привет кто то другой")
-
-
-
-
-// Запросить имя пользователя
-// Если админ -- здраствуйте администратор
-// если не админ -- привет, пользователь
-
-// let name = prompt("Введите ваше имя")
-// if(name == "админ")
-//     alert("здравствуйте администратор")
-// else
-//     alert("привет " + name)
-
-
-
-
-
-// let money = 20000
-
-// if (money > 0 И меньше чем 5000)
-// немного
-// if (money > 5000 И меньше чем 100000)
-// прилично
-// if (money > 100000)
-// дофигша
-
-
-//     let money = 20000
-
-// if (money > 0 и меньше 5000 )
-//     alert("немного")
-// if (money > 5000)
-//     alert("прилично")
-// if (money > 100000)
-//     alert("дофигша")
-
-
-
-
-// let userName = "Vasya"
-// if (userName == "Vasya" || userName == "Kolya")
-// {
-//     alert("Доступ разрешен")
-//     alert("Доступ разрешен")
-//     alert("Доступ разрешен")
-//     alert("Доступ разрешен")
-// }   
-// else
-// {
-//     alert("Доступ запрещен")
-//     alert("Доступ запрещен")
-//     alert("Доступ запрещен")
-// }
-// Проверить или у пользователя Тестер есть вип статус?
-// если да получаем сообщение Добро пожаловать
-// Иначе получаем сообщение "Уходите"
-
-// let status ="VIP"
-// let userName = "Tester"
-
-// if (status == "VIP" && userName == "Tester")
-// {
-//         alert("Добро пожаловать")
-// }
-// else 
-// {
-//         alert ("Уходите")
-// }
-
-// let names = ["Вася", "Петя", "Коля", "Женя"]
-// names[0] = '1231231j23h'
-// console.log(names)
-
-// names[3] = 'dufhgsuy'
-// console.log(names)
-// console.log(names[2])
-// names[3] = 'Женя2'
-// console.log(names)
-// names[7] = 'Маша'
-// console.log(names)
-
-// let num = [20,30,23123,6341,27615,2]
-// let num2 = num[0] + num[1] + num[2] + num[3] + num[4] + num[5]
-// console.log(num2)
-
-// console.log("Начало");for(i=0;i<3;i=i+1){console.log("привет")}
-
-//jQuery("что то там")
-
-//console.log("Начало")
-
-//for (i = 0; i < 3; i = i+1) 
-// {
-//     console.log("привет")
-// }
-
-// console.log("После цикла")
-
-// Цены товаров
-//let prices = [200, 10, 500, 1999, 200]
-// Количество товаров
-//let count = [10, 5, 3, 1, 4]
-// Товары
-//let itemNames = ["котлета", "жвачка", "икра", "чайник", "Кофе"]
-// Получаем итоговую цену
-// let totalPrice = 
-// prices[0] * count[0] + 
-// prices[1] * count[1] + 
-// prices[2] * count[2] + 
-// prices[3] * count[3] +  
-// prices[4] * count[4]
-
-
-
-// Спрашивает есть ли скид. карта
-// let bonuscard = confirm("у вас есть скидочная карта?")
-// console.log("В переменной bonuscard сейчас " , bonuscard)
-
-// Если нажимаешь "ok" 
-// if (bonuscard == true) 
-// {
-//     // То тебе дается скидка 20%
-//     totalPrice = totalPrice - (totalPrice * 0.20 )    
-// }
-
-// // Показываем в консоль купленный товар и цену
-// // console.log("Вы купили "+ count[0] + " " + itemNames[0]+ ", "  + " " + count[1]+ " " + itemNames[1]+  ", " + " " + count[2]+" "+ itemNames[2] +  ", " + " " + count[3] + " " + itemNames[3] + "." + " " + "И все вместе стоило"
-// // +" "+ totalPrice + " рублей" )
-// console.log(`Вы купили ${count[0]} ${itemNames[0]}, ${count[1]} ${itemNames[1]}, ${count[2]} ${itemNames[2]}, ${count[3]} ${itemNames[3]}, ${count[4]} ${itemNames[4]}. И все вместе стоило ${totalPrice} рублей`)
-// //console.log("Вы купили "+ count[0] + " " + itemNames[0]+ ", "  + " " + count[1]+ " " + itemNames[1]+  ", " + " " + count[2]+" "+ itemNames[2] +  ", " + " " + count[3] + " " + itemNames[3] + "." + " " + "И все вместе стоило"
-// //+" "+ totalPrice + " рублей" )
-
-// let kolb = 50
-
-// console.log("Вы купили " + kolb + " колбасы")
-
-//"Вы купили Н колбасы, М Жвачки, ..... и все вместе стоило ???Руб"
-
-// Цыкл
-// ДЗ написать 6 комментариев которые обясняют строчки
-// Массив с переменной nums
-// var nums = [3, 5, 6, 65]
-// // Объявляет переменную sum со значением 0
-// // объявляем переменную в которую будем записывать сумму числел
-// // переменная для суммы
-// // переменная с суммой чисел
-// var sum = 0
-// // Выполняет цыкл 4 (nums.length раз)
-// for (i = 0; i < nums.length; i = i + 1) 
-// // Складывает перменную sum и массив nums и записываем в сум
-// {
-//   sum = sum + nums[i]  
-// }
-// // Показывает в консоль Сумма элементов и что получилось в sum
-// console.log("Сумма элементов" , sum)
-// // Показывает в консоль Всего элементов в массиве и максимальное количество элементов в массиве
-// console.log("Всего элементов в массиве ", nums.length)
-
-
-
-
-// var nums = ["Вася", "Петя", "Коля",  "Дима"]
-// var sum = ""
-
-
-
-//  sum = sum + 10
-
-// for (i = 0; i < nums.length; i = i + 1) 
-// {
-//   // если И === 3
-// 	// if ( i == 0 || i == 1 || i == 2  )  
-//   if (i < 3)
-//   	sum = sum + nums[i] + ", "
-//   else
-//     sum = sum + nums[i] + "."  
-// }
-
-// console.log("Сумма элементов" , sum)
-// console.log("Всего элиментов в массиве ", nums.length)
-
-// if(a > 50)
-// if(name == "Vasya")
-// || ---- или
-// && ---- И
-//  console.log("Привет")
-
-// var array = [1, 2, 3, 4, 5]
-
-// for (i = 0; i < 5; i = i + 1)
-// {
-//   document.write(array[i] + '<br>');
-// }
-
-// 2 * 3 
-// 6 * 4
-// 24 * 5
-
-// произведение из всех элиментов массива
-//  var array = [2, 3, 4, 5, 100, 9999999]
-//  var sum = array[0] 
-
-//  for (i = 1; i < array.length ; i++)
-//  {
-//    sum = sum * array[i] 
-//  }
-
-// 2 4 6 8 10
-//var nums = []
-// var sum = 0
-// // sum = sum +1
-// // sum = sum +2
-// // sum = sum +3
-// // sum = sum +4
-
-// for (i = 0; i < 5; i = i + 1)
-// 	sum = sum  + i
-
-
-
-
-// console.log(array)
-
-
-// let premium = 1;
-// let xp;
-
-// if(premium == 1)
-//     xp = 2;
-// else
-//     xp = 1;
-
-
-
-
-// if( age >= 18)
-//     одна страница
-// else
-//     другая страница
-
-// введи число. Проверь оно больше чем 50 или нет? .
-//Выведи "да число больше 50" или "нет число меньше чем 50"
-
-// let levelXp =  2000
-
-// if(levelXp >= 2000)
-//     alert("levelup")
-// else
-//     alert("ничего")
-
-
-// let pleyerHP = 1
-
-// if(playerHP < 1)
-//     // умер
-// else
-//     // ничего
-
-
-
-
 // при нажатии на ДНС убирает верхнюю панельку
 $(".logo").on("click", function () {
     $('.div-one').css("display", "none")
@@ -414,18 +25,6 @@ $('.main').on("click", function () {
     // Выключает меню
     $('.notises-wrap').css("display", "none")
 })
-
-// // Нажимая на экран ( все под верхней панелькой) 
-// document.querySelector(".main").addEventListener('click', function(){
-//     // Выключает меню
-//     document.querySelector(".notises-wrap").style.display = "none"
-// })
-
-// $(".div-two").on("click", function(){
-//     $(".div-two").css("font-size","10px")
-//     $(".div-two").css("transition","0.5s")
-// })
-
 
 // получили высоту блока в самом начале работы страницы
 var initialHeight = $(".div-two").css("height")
@@ -477,39 +76,7 @@ $(document).ready(function () {
         $(".div-one").css("opacity", "1")
     }, allTime + 500);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
-
-
-
-
-
-// уберает текст, который оказывается за блоком
-// $(".div-two").css("overflow","hidden")
-// Добавляет плавность
-// $(".div-two").css("transition","0.5s")
-// Уберает половину блока
-// $(".div-two").css("height","45px")
-// $(".div-two").css("padding-bottom","0px")
-//var height = $(".div-two").css("height") 
-
-//console.log(height)
-
 
 var turbo = 0.1
 
@@ -519,7 +86,6 @@ setInterval(function () {
     // прибавляется секунда
     var sec = $(".second").text()
     sec = Number(sec) + 1
-
 
     // приписываем ноль к секундам меньше 10
     if (sec < 10) {
@@ -549,24 +115,6 @@ setInterval(function () {
     }
     else { }
 }, 1000 * turbo);
-
-// каждые 60 секунд
-//   setInterval(function() {
-//     // прибавляется минута
-//     var time = $(".minute").text() 
-//     time = Number(time) + 1
-
-//     if(time < 10){
-//         $(".minute").text("0" + time)
-//     }
-//     else {
-//         $(".minute").text(time)
-//     }
-
-//     // сбрасывает секунды до 0
-//     $(".second").text(0)
-
-//   }, 60000 * turbo );
 
 // при клике по сердечку
 $(".heart").on("click", function () {
@@ -605,11 +153,6 @@ $(".chart").on("click", function () {
 })
 
 
-//$(".button").on("click", function(){
-//   alert("8")
-//})
-
-
 $(".number").on("click", function () {
     // this это то куда ты нажимаешь
     console.log("Это THIS == ", this)
@@ -617,22 +160,13 @@ $(".number").on("click", function () {
     // то что находиться в диве this (это то куда ты нажимаешь)
     console.log("Это THIS.иннерТекст == ", this.innerText)
 
-
-
-
-    //console.log( $(".pervoe").text() ) 
-
-
     // создает переменную цифра и записывает в нее то число,  что находится в том куда нажимаешь
     // цифра -- цифра из кнопки в которую нажали
     var цифра = this.innerText
 
     // firstNumber -- определяеться что находиться в "первое"
-    var firstNumber = $(".pervoe").text()
-    // // console.log(firstNumber)
+    var firstNumber = $(".pervoe").text() 
     var secondNumber = $(".vtoroye").text()
-
-
 
     // если операция == пустоте (т.е. мы не поставили опреацию)
     if ($(".operation").text() == "") {
@@ -660,15 +194,7 @@ $(".number").on("click", function () {
             // записываем в ".второе" то что находиться в втором числе + еще цифра в которую мы нажали
             $(".vtoroye").text(secondNumber + цифра)
         }
-    }
-
-    // if(num > 10)
-    // if(name1 == "Vasya")
-    // if(status1 != 200)
-
-    // // записывает на первое место в калькуляторе введенную цифру
-    // $(".pervoe").text(цифра)
-
+    }  
 })
 
 $(".znak").on("click", function () {
@@ -720,14 +246,6 @@ $(".reset").on("click", function () {
     $(".vtoroye").text("")
 })
 
-
-
-
-
-// $(".button2").on("click", function () {
-//     console.log(this)
-// })
-
 // при клике на кнопку назад
 $(".nazad").on("click", function () 
 {
@@ -753,28 +271,10 @@ $(".nazad").on("click", function ()
     }
 })
 
-//если второе число равняеться пустоте
-// if ( $(".vtoroye").text() == "" )
-
-
-// второго числа нет  ======  если второе число равняеться пустоте
-
-
-
-// если znak есть и второго числа нет
-
-
-
-
-
-
-
-
 function стираемОперацию() {
     // при стирании операции будет пустота
     $(".operation").text("")
 }
-
 
 function отрезаетПервое() {
     // иначе отрезаем от первое
@@ -795,7 +295,3 @@ function отрезаетВторое() {
     // выводит в калькулятор отрезаное число
     $(".vtoroye").text(numberMinus1)
 }
-
-
-// console.log(firstNumber)
-// console.log(numberMinus1);
