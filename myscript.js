8f50b6dca51e0dcbd770aecf908257e98d2dcc1c
nastorvlad <llkdjjs@gmail.com>
2024-05-12 20:15:33 +0300

    Разбил проект на папки

--- myscript.js
+++ myscript.js
@@ -3,7 +3,6 @@ $(".logo").on("click", function () {
     $('.div-one').css("display", "none")
 })
 
-
 // Нажимая на профиль 
 $('.account').on("click", function () {
     // Показывает включено меню или выключено
@@ -78,44 +77,6 @@ $(document).ready(function () {
     }, allTime + 500);
 
 })
-
-var turbo = 0.1
-
-// каждую секунду
-setInterval(function () {
-
-    // прибавляется секунда
-    var sec = $(".second").text()
-    sec = Number(sec) + 1
-
-    // приписываем ноль к секундам меньше 10
-    if (sec < 10) {
-        $(".second").text("0" + sec)
-    }
-    else {
-        $(".second").text(sec)
-    }
-
-    // если наступила 60-ая секунда
-    if (sec == 60) {
-        // таймер секунд возобновляется на 0
-        $(".second").text("00")
-
-        // получаем мин из текста
-        var min = $(".minute").text()
-        // прибавляем 1 минуту
-        min = Number(min) + 1
-
-        // приписываем ноль к минутам меньше 10
-        if (min < 10) {
-            $(".minute").text("0" + min)
-        }
-        else {
-            $(".minute").text(min)
-        }
-    }
-    else { }
-}, 1000 * turbo);
 
 // при клике по сердечку
 $(".heart").on("click", function () {
